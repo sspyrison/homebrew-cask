fcde1c8f1b3733e05289ed30de1eea9027b9a0d6
Scott Spyrison <sspyrison@gmail.com>
2016-04-14 16:42:54 -0500

    Fixed SF URL's on Pentaho

--- Casks/pentaho-biserver-ce.rb
+++ Casks/pentaho-biserver-ce.rb
@@ -1,8 +1,9 @@
 cask 'pentaho-biserver-ce' do
-  bigversion '6.1'
+  #bigversion '6.1'
   version '6.1.0.1-196'
   sha256 'fb2466d2d527fe8a8e2f2679ccd18f32b3a9305248eafc2ae2949745621abe8f'
-  url "http://sourceforge.net/projects/pentaho/files/Business%20Intelligence%20Server/#{bigversion}/biserver-ce-#{version}.zip/download"
+  #url "http://sourceforge.net/projects/pentaho/files/Business%20Intelligence%20Server/#{bigversion}/biserver-ce-#{version}.zip/download"
+  url "http://downloads.sourceforge.net/sourceforge/pentaho/biserver-ce-#{version}.zip"
 
   name 'Pentaho Business Analytics Platform'
   homepage 'http://community.pentaho.com/'
--- Casks/pentaho-prd-ce.rb
+++ Casks/pentaho-prd-ce.rb
@@ -1,7 +1,9 @@
 cask 'pentaho-prd-ce' do
+#  bigversion '6.1'
   version '6.1.0.1-196'
   sha256 '7e34e9a4bfe006386b13756112f152813896ca8b35b7cf569f54c94d21056e6b'
-  url "http://sourceforge.net/projects/pentaho/files/Report%20Designer/#{bigversion}/prd-ce-mac-#{version}.zip/download"
+ # url "http://sourceforge.net/projects/pentaho/files/Report%20Designer/#{bigversion}/prd-ce-mac-#{version}.zip/download"
+  url "http://downloads.sourceforge.net/sourceforge/pentaho/prd-ce-mac-#{version}.zip"
 
   name 'Pentaho Report Designer'
   homepage 'http://community.pentaho.com/'
